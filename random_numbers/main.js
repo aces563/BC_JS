8331f21985a742f0286bdcaeccb46ab17f04e388
Michael Akerele <98211469+aces563@users.noreply.github.com>
2023-01-23 12:12:35 +0200

    added new variables to random_numbers_task

--- random_numbers/main.js
+++ random_numbers/main.js
@@ -2,10 +2,12 @@ function randomNumbers(){
     const number1 = Number(prompt('Enter first number: '));
     const number2 = Number(prompt('Enter second number: '));
     const number3 = Number(prompt('Enter third number: '));
+    const addUp = number1+number2+number3
+    const multiply = number1*number2*number3
 
     if (number1 >= 0  || number2 >=0  || number3 >=0 ){
-        console.log(number1+number2+number3);
-        console.log(number1*number2*number3);
+        console.log(addUp);
+        console.log(multiply);
 
     } else if (number1 < 0 && number2 < 0 && number3 < 0 ){
         console.log('only negatives')
