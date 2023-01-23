0998264e356ade5516e91483ffc962a885d0afd0
Michael Akerele <98211469+aces563@users.noreply.github.com>
2023-01-23 10:49:33 +0200

    added odd_or_even

---
+++ odd_or_even/main.js
@@ -0,0 +1,11 @@
+function oddOrEven(){
+    const number = prompt('Enter a number ...');
+    if (number > 0 && number%2==0){
+        console.log (`${number} is even`);
+    } else {
+        console.log (`${number} is odd`);
+
+    }
+}
+
+oddOrEven()
