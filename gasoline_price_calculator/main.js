bcaaf1a4fc9b54f5e980531b4da3341661167c7d
Michael Akerele <98211469+aces563@users.noreply.github.com>
2023-01-30 11:03:47 +0200

    added styling to gasoline price calculator

--- gasoline_price_calculator/main.js
+++ gasoline_price_calculator/main.js
@@ -1,10 +1,6 @@
 
 function dailySalary(){
-    // const hours = document.getElementById('hours').value;
-    // const hourlyFee = document.getElementById('hourlyFee').value;
-    // const bonus = (50/100)*(Number(hours) * Number(hourlyFee));
-    // const theRest = (100/100)*(Number(hours) * Number(hourlyFee));
-
+   
     const price  = document.querySelector('#price').value;
     const money = document.querySelector('#money').value;
     
