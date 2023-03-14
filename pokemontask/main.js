1ea819034a5e2ea337d1b82d5f05155fe3da9ad7
Michael Akerele <98211469+aces563@users.noreply.github.com>
2023-03-14 12:11:01 +0200

    added types to pokedex

--- pokemontask/main.js
+++ pokemontask/main.js
@@ -1,27 +1,141 @@
 let characters = document.querySelectorAll(".character");
 let images = document.querySelectorAll("img");
+let characterId = document.querySelectorAll(".character-id");
+let generation = document.querySelectorAll(".generation");
+let types = document.querySelectorAll(".types")
+
+let types2 = document.querySelectorAll(".types2")
+
 
 let c = 1;
 
-fetch("https://pokeapi.co/api/v2/pokemon/")
+pokemonId = 1;
+
+pokemonCharacters = [];
+
+pokemonTypes = [];
+
+pokemonTypes2 = [];
+
+// fetch('https://pokeapi.co/api/v2/type/18/')
+// .then(response => response.json())
+// // .then(data => console.log(data.pokemon[0].pokemon.name))
+
+// .then(data => {
+//   for(i = 0; i < data.pokemon.length; i++){
+//     console.log(data.pokemon[i].pokemon.name)
+//   }
+// })
+
+// console.log(characterId[0].textContent)
+
+
+
+// fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemonId}`)
+fetch(`https://pokeapi.co/api/v2/pokemon-species/?offset=0&limit=151`) // Gen 1
+
   .then((response) => response.json())
 
-//   .then((data) => {
-//     for (const each in data.results) {
-//       console.log(data.results[each].name);
-//       characters[each].textContent = data.results[each].name;
-//       images[each].src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${c++}.svg`
+  // .then((data) => {
+  //   for (const each in data.results) {
+  //     console.log(data.results[each].name);
+  //     characters[each].textContent = data.results[each].name;
+  //     images[each].src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${c++}.svg`
 
-//     }
-//   });
+  //     characterId[each].innerHTML = pokemonId++
+
+
+  //   }
+  // });
 
 
 .then(data => {
     for(i = 0; i < characters.length; i++){
         characters[i].innerHTML = data.results[i].name
+
+        // if(!pokemonCharacters.includes(data.results[i].name)){
+          pokemonCharacters.push(data.results[i].name)
+        // }
        
-        images[i].src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${c++}.svg`
+        images[i].src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${c++}.svg`;
+
+        generation[i].textContent = 1
+
+        characterId[i].innerHTML = pokemonId++;
 
     }
 
 })
+
+//   for(i = 0; i < characters.length; i++){
+//     fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`)
+//    .then(response => response.json())
+//    .then(data => {console.log(data)})
+   
+//  }
+
+
+
+
+window.onload = () => {
+  pokemonCharacters.forEach((each) => {
+  
+    fetch(`https://pokeapi.co/api/v2/pokemon/${each}/`)
+    .then(response => response.json())
+    
+    .then(data => {
+       console.log(`${each} has ${data.types[0].type.name}`)
+
+       pokemonTypes.push(data.types[0].type.name);
+
+       for(i = 0; i < characters.length; i++){
+        types[i].innerText = pokemonTypes[i]
+      }   
+       
+       
+       
+       try{
+         console.log(`${each} has ${data.types[1].type.name}`)
+         pokemonTypes2.push(data.types[1].type.name)
+         
+
+         for(i = 0; i < characters.length; i++){
+
+          if(pokemonTypes2[i]){
+            types2[i].innerText = pokemonTypes2[i]
+
+          }
+
+          
+        }   
+ 
+         
+         
+        }
+        catch(e){
+          return null
+        }
+        
+        
+      })
+    })
+    
+    // for(i = 0; i < characters.length; i++){
+    //     // types[i].innerText = pokemonTypes[i]
+    //     console.log(pokemonTypes.length)
+    //   }
+      
+    }
+
+  
+   
+
+
+
+  
+
+
+
+
+
+
--- tricks/main.js
+++ tricks/main.js
@@ -1,81 +1,66 @@
-const backButton = document.querySelector('#backToTop');
+const backButton = document.querySelector("#backToTop");
 
-const header = document.querySelector('header');
+const header = document.querySelector("header");
 
-const mobButton = document.querySelector('#menu');
+const mobButton = document.querySelector("#menu");
 
-const nav = document.querySelector('nav ul');
+const nav = document.querySelector("nav ul");
 
-const menuItems = document.querySelectorAll('nav ul li a');
+const menuItems = document.querySelectorAll("nav ul li a");
 
-const modalButton = document.querySelector('.modal-button')
+const modalButton = document.querySelector(".modal-button");
 
-const modal = document.querySelector('.modal');
+const modal = document.querySelector(".modal");
 
-const modalCloseButton = document.querySelector('.close-button');
+const modalCloseButton = document.querySelector(".close-button");
 
-
-window.onscroll = function() {scrollFunction()};
-
-
-
+window.onscroll = function () {
+  scrollFunction();
+};
 
 const scrollFunction = () => {
-
-    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200){
-
-
-        backButton.style.display = 'block'
-        header.classList.add('bg')
-        // header.style.backgroundColor = 'white'
-        // header.style.color = '#333'
-    }
-   else{
-       backButton.style.display = 'none'
-       header.classList.remove('bg')
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    backButton.style.display = "block";
+    header.classList.add("bg");
+    // header.style.backgroundColor = 'white'
+    // header.style.color = '#333'
+  } else {
+    backButton.style.display = "none";
+    header.classList.remove("bg");
     //    header.style.backgroundColor = 'blue'
-
-
-   }
-   
-}
+  }
+};
 
 const goToTop = () => {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+};
 
 const mobMenu = () => {
+  for (const link of menuItems) {
+    link.addEventListener("click", mobMenu);
+  }
 
-    for (const link of menuItems){
-        link.addEventListener('click', mobMenu)
-
-    }
-
-    if (nav.classList.contains('responsive')){
-        nav.classList.remove('responsive')
-        
-
-
-    }
-    else {
-        nav.classList.add('responsive')
-    }
-}
+  if (nav.classList.contains("responsive")) {
+    nav.classList.remove("responsive");
+  } else {
+    nav.classList.add("responsive");
+  }
+};
 
 const modalShow = () => {
-    modal.classList.toggle('visible')
-}
+  modal.classList.toggle("visible");
+};
 
 // const hideMenu = () => {
 //     nav.classList.toggle('responsive');
 // }
 
-backButton.addEventListener('click', goToTop);
-mobButton.addEventListener('click', mobMenu);
-modalButton.addEventListener('click', modalShow)
-modalCloseButton.addEventListener('click', modalShow)
+backButton.addEventListener("click", goToTop);
+mobButton.addEventListener("click", mobMenu);
+modalButton.addEventListener("click", modalShow);
+modalCloseButton.addEventListener("click", modalShow);
 // nav.addEventListener('click', hideMenu);
- 
-
-    
